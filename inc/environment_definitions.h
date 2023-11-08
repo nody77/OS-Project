3a63a0b91c1e0775bf73c2be4c05c4775dab83ad
Manar Mohamed El-Gamal <104044303+ManarMohamed2003@users.noreply.github.com>
2023-11-08 07:19:48 +0200

    Update env struct

--- inc/environment_definitions.h
+++ inc/environment_definitions.h
@@ -108,6 +108,9 @@ struct Env {
 
 	/*2023*/
 	//TODO: [PROJECT'23.MS2 - #07] [2] USER HEAP - initialize of Env: add suitable code here
+        uint32 * start;
+	uint32 * segmentBreak ;
+	uint32 * hardLimit ;
 
 	//==================================================================================
 
