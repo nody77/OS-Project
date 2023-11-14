36a21bde6f2903807a9b32a868abd3eaa4b4dfef
nody77 <104033293+nody77@users.noreply.github.com>
2023-11-14 10:32:31 +0200

    adding page fault handler code but had not bess tested yet

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -89,19 +89,34 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 		//TODO: [PROJECT'23.MS2 - #15] [3] PAGE FAULT HANDLER - Placement
 		// Write your code here, remove the panic and write your code
 		//panic("page_fault_handler().PLACEMENT is not implemented yet...!!");
-		struct FrameInfo * frame_to_be_allcoated;
-		int return_frame_allocation = allocate_frame(&frame_to_be_allcoated);
+		struct FrameInfo * frame_to_be_allocated;
+		int return_frame_allocation = allocate_frame(&frame_to_be_allocated);
 		if (return_frame_allocation == 0)
 		{
-			int return_map_allcoation = map_frame(ptr_page_directory ,frame_to_be_allcoated, fault_va,(PERM_PRESENT|PERM_USED));
+			int return_map_allcoation = map_frame(ptr_page_directory ,frame_to_be_allocated, fault_va,(PERM_PRESENT|PERM_USED));
 			if (return_map_allcoation == 0 )
 			{
-				int return_read_pageFile = pf_read_env_page(curenv , fault_va);
+				int return_read_pageFile = pf_read_env_page(curenv , (void *)fault_va);
 				if(return_read_pageFile == E_PAGE_NOT_EXIST_IN_PF)
 				{
-					// check if the page file is a stack or heap or illegal access
+					// check if the page file is illegal access
+					if((uint32 *)fault_va < curenv->start || (uint32 *)fault_va > curenv->hardLimit)
+					{
+						sched_kill_env(curenv->env_id);
+					}
 				}
-				//update the working set list to add the new frame
+				//update the page file to the new allocated mapped frame even if it did not exit for stack or heap
+				int return_update_pageFile = pf_update_env_page(curenv , fault_va , frame_to_be_allocated);
+				if (return_update_pageFile == 0)
+				{
+					struct WorkingSetElement * wseToBeAdded = env_page_ws_list_create_element(curenv,fault_va);
+					if (curenv->page_last_WS_element == NULL)
+					{
+						//update the working set list to add the new frame
+						LIST_INSERT_TAIL(&(curenv->page_WS_list) , wseToBeAdded);
+					}
+				}
+
 			}
 		}
 		//refer to the project presentation and documentation for details
