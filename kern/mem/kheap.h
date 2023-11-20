050f25a633b078e015cd86f2ac580626861b7e3e
nody77 <104033293+nody77@users.noreply.github.com>
2023-11-20 11:56:46 +0200

    implementing kmalloc

--- kern/mem/kheap.h
+++ kern/mem/kheap.h
@@ -5,19 +5,18 @@
 # error "This is a FOS kernel header; user programs should not #include it"
 #endif
 
-#include <inc/types.h>
 #include <inc/queue.h>
-
-LLIST_HEAD(Page_Allocator_Block, Block);
-struct Page_Allocator_Block block_list;
+#include <inc/types.h>
+#include <inc/memlayout.h>
+#include <inc/dynamic_allocator.h>
 
 struct Block
 {
-	//uint32 * va;
-	unsigned int size;
-	int free;
-	LIST_ENTRY(Block) prev_next_info;
+	uint8 is_free;
+	uint32 virtual_address;
+	struct FrameInfo * frame;
 };
+
 
 /*2017*/
 uint32 _KHeapPlacementStrategy;
@@ -27,6 +26,9 @@ uint32 _KHeapPlacementStrategy;
 #define KHP_PLACE_BESTFIT 	0x2
 #define KHP_PLACE_NEXTFIT 	0x3
 #define KHP_PLACE_WORSTFIT 	0x4
+
+#define KHEAP_PAGE_ALLOCATOR_SIZE (KERNEL_HEAP_MAX-(KERNEL_HEAP_START + DYN_ALLOC_MAX_SIZE + PAGE_SIZE))/PAGE_SIZE
+
 
 static inline void setKHeapPlacementStrategyCONTALLOC(){_KHeapPlacementStrategy = KHP_PLACE_CONTALLOC;}
 static inline void setKHeapPlacementStrategyFIRSTFIT(){_KHeapPlacementStrategy = KHP_PLACE_FIRSTFIT;}
@@ -45,6 +47,7 @@ static inline uint8 isKHeapPlacementStrategyWORSTFIT(){if(_KHeapPlacementStrateg
 void* kmalloc(unsigned int size);
 void kfree(void* virtual_address);
 void *krealloc(void *virtual_address, unsigned int new_size);
+void initialize_page_list();
 
 unsigned int kheap_virtual_address(unsigned int physical_address);
 unsigned int kheap_physical_address(unsigned int virtual_address);
@@ -58,6 +61,7 @@ int numOfKheapVACalls ;
 uint32 *Start;
 uint32 *SegmentBreak;
 uint32 *HardLimit;
+struct Block Page_Allocation_list[KHEAP_PAGE_ALLOCATOR_SIZE];
 //====================================================================================
 
 #endif // FOS_KERN_KHEAP_H_
