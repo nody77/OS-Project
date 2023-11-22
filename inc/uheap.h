ab991e1fe3597f0587930fa833520c5f62ece5ae
Aliaa Osama <104230881+AliaaOsamaE@users.noreply.github.com>
2023-11-22 19:17:06 +0200

    Update uheap.h

--- inc/uheap.h
+++ inc/uheap.h
@@ -1,3 +1,5 @@
+#include <inc/dynamic_allocator.h>
+
 #ifndef FOS_INC_UHEAP_H
 #define FOS_INC_UHEAP_H 1
 
@@ -10,11 +12,27 @@
 //2020
 #define UHP_USE_BUDDY 0
 
+#define UHEAP_PAGE_ALLOCATOR_SIZE (USER_HEAP_MAX-(USER_HEAP_START + DYN_ALLOC_MAX_SIZE + PAGE_SIZE))/PAGE_SIZE
+
+
+struct userBlock{
+	uint8 is_free;
+	uint32 size;
+	uint32 virtual_address;
+};
+
+struct userBlock user_Page_Allocation_list[UHEAP_PAGE_ALLOCATOR_SIZE];
+
 void *malloc(uint32 size);
 void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable);
 void* sget(int32 ownerEnvID, char *sharedVarName);
 void free(void* virtual_address);
 void sfree(void* virtual_address);
 void *realloc(void *virtual_address, uint32 new_size);
+
+void initialize_user_page_list();
+
+
+
 
 #endif
