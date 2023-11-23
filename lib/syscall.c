03e42c57e6258d721368f40dce2dd30753620d89
nody77 <104033293+nody77@users.noreply.github.com>
2023-11-23 10:02:59 +0200

    fixing the new system call

--- lib/syscall.c
+++ lib/syscall.c
@@ -334,12 +334,12 @@ void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 
 /*2023*/
 //TODO: [PROJECT'23.MS1 - #3] [2] SYSTEM CALLS - Implement these system calls
-void* sys_hardlimit(struct Env* e)
+void* sys_hardlimit()
 {
 	//Comment the following line before start coding...
 	//panic("not implemented yet");
 	// system call MS2
-	return (void *)syscall(SYS_to_get_hardlimit, (uint32) e->hardLimit,0, 0, 0, 0);
+	return (void *)syscall(SYS_to_get_hardlimit, 0 ,0, 0, 0, 0);
 }
 
 void* sys_sbrk(int increment)
