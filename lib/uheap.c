6394ca9139664307dd07a775d4f5d95cb57c38e6
mennaraslan <103905174+mennaraslan@users.noreply.github.com>
2024-02-10 23:41:14 +0200

    Add files via upload

---
+++ lib/uheap.c
@@ -0,0 +1,290 @@
+#include <inc/lib.h>
+
+//#include <kern/proc/user_environment.h>
+
+//#include <kern/proc/user_environment.h>
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+
+struct userBlock
+{
+	uint8 is_full ;
+	uint32 virtual_address;
+	uint32 size;
+};
+
+struct userBlock user_Page_Allocation_list[UHEAP_PAGE_ALLOCATOR_SIZE];
+
+int FirstTimeFlag = 1;
+void InitializeUHeap()
+
+{
+	if(FirstTimeFlag)
+	{
+#if UHP_USE_BUDDY
+		initialize_buddy();
+		cprintf("BUDDY SYSTEM IS INITIALIZED\n");
+#endif
+		FirstTimeFlag = 0;
+	}
+}
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+
+//=============================================
+// [1] CHANGE THE BREAK LIMIT OF THE USER HEAP:
+//=============================================
+//2023/
+void* sbrk(int increment)
+{
+	return (void*) sys_sbrk(increment);
+}
+
+
+/*void initialize_user_page_list()
+{
+	for(int i = 0; i<UHEAP_PAGE_ALLOCATOR_SIZE ;i+=1)
+	{
+		user_Page_Allocation_list[i].is_free = 1 ;
+	}
+
+}*/
+//=================================
+// [2] ALLOCATE SPACE IN USER HEAP:
+//=================================
+void* malloc(uint32 size)
+{
+	//cprintf("malloc 1\n");
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	InitializeUHeap();
+	if (size == 0) return NULL ;
+	//==============================================================
+	//TODO: [PROJECT'23.MS2 - #09] [2] USER HEAP - malloc() [User Side]
+	// Write your code here, remove the panic and write your code
+	//panic("malloc() is not implemented yet...!!");
+	if (size <= DYN_ALLOC_MAX_BLOCK_SIZE)
+	{
+			void * va = alloc_block_FF(size);
+			return va;
+	}
+	else
+	{
+		//cprintf("size : %d \n",size);
+		//cprintf("entered else\n");
+		uint32 * HardLimit = (uint32*)sys_hardlimit();
+		if(sys_isUHeapPlacementStrategyFIRSTFIT() == 1) //sys_isUHeapPlacementStrategyBESTFIT()
+			{
+					int index , counter = 0 , flag_frames_found = 0;
+					uint32 number_of_allocated_frames = ROUNDUP(size , PAGE_SIZE) / PAGE_SIZE;
+
+					for(index = 0 ; index < UHEAP_PAGE_ALLOCATOR_SIZE ; index+=1)
+					{
+						if(counter == number_of_allocated_frames)
+						{
+							//cprintf("Found Block 1st if\n");
+							flag_frames_found = 1;
+							break;
+						}
+						if(user_Page_Allocation_list[index].is_full == 1)
+						{
+							//cprintf("Page is not free\n");
+							counter = 0;
+						}
+						else
+						{
+
+							counter+=1;
+						}
+					}
+					if(flag_frames_found == 1)
+					{
+						//cprintf("flag_frames_found == 1\n");
+						int start_index = index - number_of_allocated_frames;
+						uint32 va = (uint32)HardLimit + PAGE_SIZE + (uint32)(PAGE_SIZE * start_index);
+						uint32 startVa = va;
+						//cprintf("startVa : %x\n",startVa);
+						for(int i = start_index ; i < (index); i += 1)
+						{
+							if(i == start_index)
+							{
+								user_Page_Allocation_list[i].size = size;
+							}
+							else
+							{
+								user_Page_Allocation_list[i].size = 0;
+							}
+
+							    user_Page_Allocation_list[i].is_full = 1;
+							    user_Page_Allocation_list[i].virtual_address = va;
+								va += PAGE_SIZE;
+								//cprintf("va : %x \n",va);
+						}
+						sys_allocate_user_mem(startVa,size);
+						//cprintf("malloc 2\n");
+						return (void *)startVa;
+
+					}
+			}
+	}
+	//cprintf("malloc 2\n");
+	return NULL;
+	//Use sys_isUHeapPlacementStrategyFIRSTFIT() and	sys_isUHeapPlacementStrategyBESTFIT()
+	//to check the current strategy
+
+}
+
+//=================================
+// [3] FREE SPACE FROM USER HEAP:
+//=================================
+void free(void* virtual_address)
+{
+	//cprintf("free 1\n");
+	uint32 * SegmentBrk = (uint32*)sys_sbrk(0);
+	//uint32 * HardLimit = (uint32*)sys_hardlimit();
+	uint32 * HardLimit = (uint32*)sys_hardlimit();
+	uint32 * Start = (uint32*)sys_uheapStart();
+
+	if ((virtual_address == ((void *)HardLimit + PAGE_SIZE) || virtual_address > ((void *)HardLimit + PAGE_SIZE)) && virtual_address < (void*) USER_HEAP_MAX)
+	{
+		uint32 size;
+		uint32 index = 0;
+		for(index = 0 ; index < UHEAP_PAGE_ALLOCATOR_SIZE ; index+=1)
+				{
+					if(user_Page_Allocation_list[index].virtual_address == (uint32)virtual_address)
+					{
+						size = user_Page_Allocation_list[index].size;
+						break;
+					}
+				}
+
+				uint32 number_of_allocated_frames = ROUNDUP( user_Page_Allocation_list[index].size, PAGE_SIZE) / PAGE_SIZE;
+				uint32 end_index = number_of_allocated_frames + index;
+				for(int i = index; i < end_index; i+=1)
+				{
+					user_Page_Allocation_list[i].is_full = 0;
+					user_Page_Allocation_list[i].size = 0;
+					user_Page_Allocation_list[i].virtual_address = 0;
+				}
+				sys_free_user_mem((uint32)virtual_address,size);
+			//cprintf("free 2\n");
+				return;
+
+	}
+	else if(virtual_address < (void *)SegmentBrk)
+	{
+		if(virtual_address >= (void *)USER_HEAP_START){
+			free_block(virtual_address);
+			//cprintf("free 2\n");
+		}
+
+	}
+	else
+	{
+		panic("invalid address\n");
+	}
+}
+
+
+//=================================
+// [4] ALLOCATE SHARED VARIABLE:
+//=================================
+void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	InitializeUHeap();
+	if (size == 0) return NULL ;
+	//==============================================================
+	panic("smalloc() is not implemented yet...!!");
+	return NULL;
+}
+
+//========================================
+// [5] SHARE ON ALLOCATED SHARED VARIABLE:
+//========================================
+void* sget(int32 ownerEnvID, char *sharedVarName)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	InitializeUHeap();
+	//==============================================================
+	// Write your code here, remove the panic and write your code
+	panic("sget() is not implemented yet...!!");
+	return NULL;
+}
+
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+//=================================
+// REALLOC USER SPACE:
+//=================================
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to malloc().
+//	A call with new_size = zero is equivalent to free().
+
+//  Hint: you may need to use the sys_move_user_mem(...)
+//		which switches to the kernel mode, calls move_user_mem(...)
+//		in "kern/mem/chunk_operations.c", then switch back to the user mode here
+//	the move_user_mem() function is empty, make sure to implement it.
+void *realloc(void *virtual_address, uint32 new_size)
+{
+	//==============================================================
+	//DON'T CHANGE THIS CODE========================================
+	InitializeUHeap();
+	//==============================================================
+
+	// Write your code here, remove the panic and write your code
+	panic("realloc() is not implemented yet...!!");
+	return NULL;
+
+}
+
+
+//=================================
+// FREE SHARED VARIABLE:
+//=================================
+//	This function frees the shared variable at the given virtual_address
+//	To do this, we need to switch to the kernel, free the pages AND "EMPTY" PAGE TABLES
+//	from main memory then switch back to the user again.
+//
+//	use sys_freeSharedObject(...); which switches to the kernel mode,
+//	calls freeSharedObject(...) in "shared_memory_manager.c", then switch back to the user mode here
+//	the freeSharedObject() function is empty, make sure to implement it.
+
+void sfree(void* virtual_address)
+{
+	// Write your code here, remove the panic and write your code
+	panic("sfree() is not implemented yet...!!");
+}
+
+
+//==================================================================================//
+//========================== MODIFICATION FUNCTIONS ================================//
+//==================================================================================//
+
+void expand(uint32 newSize)
+{
+	panic("Not Implemented");
+
+}
+void shrink(uint32 newSize)
+{
+	panic("Not Implemented");
+
+}
+void freeHeap(void* virtual_address)
+{
+	panic("Not Implemented");
+
+}
